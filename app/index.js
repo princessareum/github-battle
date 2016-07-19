f34193a80d424d50db7c76dd00ad8814f4657fa2
A-Reum Jung <areumjung1987@gmail.com>
2016-07-19 13:25:07 -0600

    first commit

---
+++ app/index.js
@@ -0,0 +1,53 @@
+var USER_DATA = {
+  name: 'A-Reum Jung',
+  username: 'princessareum',
+  image: 'https://avatars2.githubusercontent.com/u/17772662?v=3&s=460'
+}
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avartar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
+
+ReactDOM.render(
+  <Avartar user={USER_DATA}/>,
+  document.getElementById('app')
+)
