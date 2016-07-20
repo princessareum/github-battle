42efc2b272b7575384527dff30b3f5a0b654a6a1
A-Reum Jung <areumjung1987@gmail.com>
2016-07-19 21:06:58 -0600

    rm cached files from git

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,17 @@
+var axios = require('axios');
+
+var id = "YOUR_CLINET_ID";
+var sec = "YOUR_SECRET_ID";
+var param = "?client_id=" + id + "&client_secret" + sec
+
+function getUserInfo(username){
+  return axios.get('https://api.github.com/users/' + username + param)
+}
+
+var helpers = {
+  getPlayersInfo: function(){
+    //fetch some data from github
+  }
+};
+
+module.exports = helpers;
